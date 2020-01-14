40f5b012245f023046a33263c9bc9485311b3ea0
Ciaran Bent [K00221230] <k00221230@student.lit.ie>
2020-01-14 22:43:16 +0000

    Added new upload script; Added print to show incoming Event;
    - RetailCrawler/index.py
      - Added import for 'os'.
      - Added print statements to log the current actionable item.
    
    - uparchive.sh
      - Added script to generate a new ZIP, and then upload it.

--- RetailCrawler/index.py
+++ RetailCrawler/index.py
@@ -1,9 +1,18 @@
 import json
+import os
 
 
 def lambda_handler(event, context):
     # TODO: implement
+
+    # Print some things
+    print('## ENVIRONMENT VARIABLES')
+    print(os.environ)
+    print('## EVENT')
+    print(event)
+
     return {
         'statusCode': 200,  # OK
         'body': json.dumps('Hello from the first HWMS Lambda Function!')
+
     }
---
+++ uparchive.sh
@@ -0,0 +1,15 @@
+# Script to create a ZIP package of the current development of HWMS
+# retail crawler for uploading as a Lambda function.
+
+echo "Adding Dependancies..."
+cd "RetailCrawler/.venv_hwms/lib/python3.7/site-packages"
+zip -r9 ${OLDPWD}/hwms_retailPriceCrawler.zip .
+echo "Adding Dependancies...DONE!"
+
+echo "Adding Source..."
+cd ${OLDPWD}/RetailCrawler
+zip -g ../hwms_retailPriceCrawler.zip *py
+echo "Adding Source...DONE!"
+
+aws lambda update-function-code --function-name hwms_retailPriceCrawler --zip-file fileb://../hwms_retailPriceCrawler.zip
+
