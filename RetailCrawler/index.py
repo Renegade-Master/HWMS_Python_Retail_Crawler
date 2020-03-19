c0a01273f815927dadfbe101670b52634194c0a8
Ciaran Bent [K00221230] <k00221230@student.lit.ie>
2020-03-19 21:03:11 +0000

    Added test file; Attempting to integrate CI;
    - 
      -

--- RetailCrawler/index.py
+++ RetailCrawler/index.py
@@ -46,6 +46,7 @@ def lambda_handler(event, context):
     print('## PHASE 03')
     dynamodb = boto3.resource('dynamodb', 'eu-west-1')
     results_table = dynamodb.Table('SearchQueryResponse-2aiyr2wthzdrxas7zpxcowy2gm-hwmstest')
+    search_results = cw.get_results()
 
     # Disable the following command to prevent results being posted to DynamoDB
     results_table.put_item(
@@ -53,7 +54,7 @@ def lambda_handler(event, context):
             'id': str(request_id),
             '__typename': 'SearchQueryResponse',
             'createdAt': get_current_datetime(),
-            'result': cw.get_results(),
+            'result': search_results,
             'updatedAt': get_current_datetime()
         }
     )
@@ -62,13 +63,22 @@ def lambda_handler(event, context):
 
     return {
         'statusCode': 200,  # OK
-        'body': dumps('The Web hath been Crawled!')
+        'body': dumps('The Web hath been Crawled!'),
+        'crawlResults': dumps(search_results)
     }
 
 
 # Both of the following functions are for offline testing.
 def __main__():
-    aws_event = "{'Records': [{'eventID': '67aa50f9a78d16d372b673cfec5e6e19', 'eventName': 'INSERT', 'eventVersion': '1.1', 'eventSource': 'aws:dynamodb', 'awsRegion': 'eu-west-1', 'dynamodb': {'ApproximateCreationDateTime': 1579040208, 'Keys': {'id': {'S': '1111111111111'}}, 'NewImage': {'createdAt': {'S': '2020-01-14T22:16:48.341Z'}, 'item': {'S': 'CPU Intel i5 8600K'}, '__typename': {'S': 'SearchQueryRequest'}, 'prediction': {'BOOL': False}, 'id': {'S': '1579111412732'}, 'updatedAt': {'S': '2020-01-14T22:16:48.341Z'}}, 'SequenceNumber': '30337100000000002705939892', 'SizeBytes': 157, 'StreamViewType': 'NEW_AND_OLD_IMAGES'}, 'eventSourceARN': 'arn:aws:dynamodb:eu-west-1:227389701406:table/SearchQueryRequest-5fsl2xomebd6tmxdjt3xsocctm-testenv/stream/2020-01-08T12:31:34.022'}]}"
+    aws_event = "{'Records': [{'eventID': '67aa50f9a78d16d372b673cfec5e6e19', 'eventName': 'INSERT', 'eventVersion': " \
+                "'1.1', 'eventSource': 'aws:dynamodb', 'awsRegion': 'eu-west-1', 'dynamodb': {" \
+                "'ApproximateCreationDateTime': 1579040208, 'Keys': {'id': {'S': '1111111111111'}}, 'NewImage': {" \
+                "'createdAt': {'S': '2020-01-14T22:16:48.341Z'}, 'item': {'S': 'CPU Intel i5 8600K'}, '__typename': {" \
+                "'S': 'SearchQueryRequest'}, 'prediction': {'BOOL': False}, 'id': {'S': '1579111412732'}, " \
+                "'updatedAt': {'S': '2020-01-14T22:16:48.341Z'}}, 'SequenceNumber': '30337100000000002705939892', " \
+                "'SizeBytes': 157, 'StreamViewType': 'NEW_AND_OLD_IMAGES'}, 'eventSourceARN': " \
+                "'arn:aws:dynamodb:eu-west-1:227389701406:table/SearchQueryRequest-5fsl2xomebd6tmxdjt3xsocctm-testenv" \
+                "/stream/2020-01-08T12:31:34.022'}]} "
 
     lambda_handler(aws_event, 'null')
 
